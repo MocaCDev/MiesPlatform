01e49dd4e745841fafa5a6e3ef46c4139a4dad32
GetStuffDone <36897041+ARACADERISE@users.noreply.github.com>
2019-10-03 21:51:39 -0500

    Update start_server.py

--- start_server.py
+++ start_server.py
@@ -1,6 +1,7 @@
 import subprocess, os, json, socket
 from colorama import *
 from time import sleep as s
+from start_client import *
 
 # This will setup connection from client
 FROM = ""
@@ -8,9 +9,19 @@ HAS_CONNECTED = False
 MSG = ""
 CON_TO = ""
 STARTED = False
+RECEIVED_MSG = ""
+#IP = ""
+
+def receiveMSG(msg):
+  global RECEIVED_MSG
+
+  if len(msg) > 0:
+    RECEIVED_MSG = msg
+  else:
+    pass
 
 def makefile(data):
-  with open('start_server.json','w') as file:
+  with open('server.json','w') as file:
     to_json = json.dumps(data,sort_keys=False)
     file.write(to_json)
     file.close()
@@ -18,14 +29,13 @@ def makefile(data):
 def gather(from_,msg,con_to):
   global FROM, MSG, HAS_CONNECTED, CON_TO
   
-  FROM += f"Established connection with ('{from_}')<:>('{socket.gethostname()}')\n"
-  MSG += f"Connection {from_} gives: {msg}\n"
+  FROM += f"('{from_}')<:>('{socket.gethostname()}')\n"
+  MSG += f"Message from {FROM}: {msg}"
   HAS_CONNECTED = True
   CON_TO = con_to
 
-def gathered():
+def received_msg():
   global FROM, MSG
-  print(f'{FROM}')
   print(f'{MSG}')
 
 def client_started(has_started):
@@ -48,17 +58,28 @@ class server:
   def start(self):
     data = {'ip':self.ip,'port':self.port}
     makefile(data)
-    if os.path.exists('start_server.json'):
+    if os.path.exists('server.json'):
       self.server_has_started = True
   
+  def _conName_(self):
+    return FROM
+  
+  def _receive_(self,con_msg):
+    self.msg_to_show = con_msg
+  
   def _establish_(self,accepting):
+    #global IP
+    #IP = self.ip
     if self.server_has_started:
       print(Fore.GREEN+Style.BRIGHT+'[+]' + Fore.WHITE + ' Server established..awaiting other connections')
+      server_ready(self.server_has_started,accepting,self.ip)
     while self.server_has_started:
       if self.client_started:
         if self.has_connected and self.has_connected_to == self.ip:
           s(3)
-          gathered()
+          print(self.msg_to_show)
+          received_msg()
+          receiveMSG()
           self.has_connected = False
       else:
         continue
