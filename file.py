c40d2398beca501c5c0333b1b444f74ff10af5fb
GetStuffDone <36897041+ARACADERISE@users.noreply.github.com>
2019-10-12 12:00:41 -0500

    Update file.py

--- file.py
+++ file.py
@@ -1,18 +1,25 @@
 from mies_net import *
 import os
 from time import sleep as s
+from colorama import Fore, Style
+from mies_net import mies_network
+from looting import *
 
 s = mies_network()
 
 s._set_ip_()
 
-type_ = input('Create file or use existing[c,u]: ')
+type_ = input(Fore.CYAN + '\nCreate file or use existing[c,u]: ')
 
 if type_ == 'c':
-  file_to_create = input('Create file: ')
+  file_to_create = input(Fore.BLUE + 'Create file: ')
+  if os.path.exists(file_to_create):
+    raise Exception("Path " + os.path.abspath(file_to_create) + " already exists\n\n" + "File Reader Gathered:\n " + open(os.path.abspath(file_to_create),'r').read())
   s._gather_(create_path=file_to_create)
   s._establish_('created ' + os.path.abspath(file_to_create))
 if type_ == 'u':
-  file_name = input('Existing Filename: ')
+  file_name = input(Fore.BLUE + 'Existing Filename: ')
   s._gather_(PATH=file_name)
   s._establish_('connected to ' + os.path.abspath(file_name))
+
+s._START_CONNECTION_()
