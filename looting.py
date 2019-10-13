738ff05e53fe538a561e542053354b120cc8dc0d
GetStuffDone <36897041+ARACADERISE@users.noreply.github.com>
2019-10-12 23:35:36 -0500

    Update looting.py

--- looting.py
+++ looting.py
@@ -1,10 +1,16 @@
-"This file is the looting point for connections to and from the IP address"
+"This file is the looting point for connectiong to and from the IP address"
 import os, json, subprocess
 from colorama import Fore,Style
 from time import sleep as s
 
 # This will be the connection names
 CONNECTION_WITH = []
+# This will get extra info from the IP address
+DATA = ""
+
+def _get_extra_(extra):
+  global DATA
+  DATA = extra
 
 class connect:
   
@@ -21,9 +27,9 @@ class connect:
     This will gather the IP address of which the user wants to connect to
     depending on whether or not the given IP address has a linked file to it
     """
-    print(Fore.GREEN+Style.BRIGHT+'[+]'+Fore.WHITE+' Setup IP done...\n\n')
+    print(Fore.GREEN+Style.BRIGHT+'[+]'+Fore.WHITE+' Setup IP done...')
     s(4)
-    print(Style.BRIGHT+Fore.RED+'[-]'+Fore.WHITE+' Connection to IP not complete..\n\n\nLoading...')
+    print(Style.BRIGHT+Fore.RED+'[-]'+Fore.WHITE+' Connection to IP not complete..\n\nLoading...')
     s(5)
     
     os.system('clear')
@@ -41,10 +47,17 @@ class connect:
 
 
   def _connect_(self):
+    global DATA
     """
     This will follow through with the status of the connection
     """
-    if self.con_info['con_to_file_through_ip'] == self.IP:
-      print('connection complete. connection with ' + self.con_info['con_to_file_through_ip'] + ' to ' + self.data['use'][self.con_info['con_to_file_through_ip']+'_con_to_file_'])
+    # Opening data.json if it exists
+    if os.path.exists(os.path.abspath('data.json')):
+      is_a_path = True
+    if is_a_path == True and self.con_info['con_to_file_through_ip'] in DATA:
+      print('connection complete. connection with ' + str(self.con_info['con_to_file_through_ip']) + ' to ' + str(DATA[self.con_info['con_to_file_through_ip']][0]))
     else:
-      print("Can't connect to a file through ip " + self.con_info['con_to_file_through_ip'])
+      if self.con_info['con_to_file_through_ip'] == self.IP:
+        print('connection complete. connection with ' + self.con_info['con_to_file_through_ip'] + ' to ' + self.data['use'][self.con_info['con_to_file_through_ip']+'_con_to_file_'])
+      else:
+        print("Can't connect to a file through ip " + self.con_info['con_to_file_through_ip'])
