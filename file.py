9ccc0960868c62ebd348910a89df3059ef282ff2
GetStuffDone <36897041+ARACADERISE@users.noreply.github.com>
2019-10-24 23:40:30 -0500

    Update file.py

--- file.py
+++ file.py
@@ -6,6 +6,11 @@ from mies_net import mies_network
 
 s = mies_network()
 
+def _raise_using_error_(t):
+  raise Exception('These names are data files for the application:\ndata.txt\ndata.json\ncomplete_connection.json\n\nOops sorry, we cannot use ' + t + ' as a data file')
+def _raise_create_error_(t):
+  raise Exception('These names are data files for the application:\ndata.txt\ndata.json\ncomplete_connection.json\n\nOops sorry, we cannot create a file of name ' + t)
+
 s._make_fields_()
 
 MAIN_PATH = os.environ.get('HOME')
@@ -17,6 +22,9 @@ if not os.path.isfile(os.path.abspath('data.json')) or not os.path.isfile(os.pat
 
   if type_ == 'c':
     file_to_create = input(Fore.BLUE + 'Create file: ')
+    if file_to_create == 'data.txt':_raise_create_error_(file_to_create)
+    if file_to_create == 'data.json':_raise_create_error_(file_to_create)
+    if file_to_create == 'complete_connection.json':_raise_create_error_(file_to_create)
     check_path = os.path.join(MAIN_PATH,file_to_create)
     if os.path.exists(check_path):
       raise Exception("Path " + os.path.abspath(file_to_create) + " already exists\n\n" + "File Reader Gathered:\n " + open(os.path.abspath(file_to_create),'r').read())
@@ -31,6 +39,9 @@ if not os.path.isfile(os.path.abspath('data.json')) or not os.path.isfile(os.pat
     else:
       os.system(f'clear && cd {MAIN_PATH} ' + '&& echo "\n" && ls')
     file_name = input(Fore.BLUE + 'Existing Filename: ')
+    if file_name == 'data.txt':_raise_using_error_(file_name)
+    if file_name == 'data.json':_raise_using_error_(file_name)
+    if file_name == 'complete_connection.json':_raise_using_error_(file_name)
     _PATH_ = os.path.join(MAIN_PATH,file_name)
     s._gather_(PATH=_PATH_)
     s._establish_('connected to ' + os.path.abspath(file_name),s._return_ip_())
