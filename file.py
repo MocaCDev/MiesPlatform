7f69846ff50b3bfd04592117b906bbb1e3b6e6cd
GetStuffDone <36897041+ARACADERISE@users.noreply.github.com>
2019-11-16 17:31:55 -0600

    Update file.py

--- file.py
+++ file.py
@@ -34,7 +34,8 @@ if not os.path.isfile(os.path.abspath('data.json')) or not os.path.isfile(os.pat
     os.system(f'clear && cd {MAIN_PATH} && echo "\n" && ls')
     folder_name = input('\nName of folder which contains the file: ')
     if '.gf' in folder_name:
-      os.system('cd MiesPlatform')
+      op = open(MAIN_PATH + '/' + open(MAIN_PATH + '/MiesPlatform/Con_Files' + '/' + folder_name,'r').read()).read()
+      os.system(f'echo {op}')
       if os.path.isfile(MAIN_PATH + '/MiesPlatform/Con_Files' + '/' + folder_name):
         op = open(MAIN_PATH + '/MiesPlatform/Con_Files' + '/' + folder_name,'r').read()
         MAIN_PATH = MAIN_PATH + op
